291c51319ae1e8470f345c9c6fdac429171e1df6
iamtonny <pliomixxx@gmail.com>
2017-09-22 11:42:12 +0300

    Changed variables in connect method

--- utils/connector.py
+++ utils/connector.py
@@ -2,7 +2,30 @@
 # coding=utf-8
 
 from mysql.connector import MySQLConnection, Error
-from queries import auth_query, \
+
+
+def connect(host='localhost', database=None, user=None, password=None):
+	""" Connect to MySQL database """
+	connection = None
+	try:
+		connection = MySQLConnection(password=password, user=user, host=host, database=database)
+
+	finally:
+		return connection
+
+
+def disconnect(connection=None):
+	try:
+		if connection is not None:
+			connection.close()
+			return True
+	except Error:
+		return False
+
+
+if __name__ == '__main__':
+
+	from queries import auth_query, \
 					create_user_query, \
 					add_note_query, \
 					get_all_notes_query, \
@@ -16,26 +39,6 @@ from queries import auth_query, \
 					update_note_by_id_query
 
 
-def connect(host='localhost', database=None, user=None, password=None):
-	""" Connect to MySQL database """
-	connected = None
-	try:
-		connected = MySQLConnection(password=password, user=user, host=host, database=database)
-
-	finally:
-		return connected
-
-
-def disconnect(connection=None):
-	try:
-		if connection is not None:
-			connection.close()
-			return True
-	except Error:
-		return False
-
-
-if __name__ == '__main__':
 	conn = connect('localhost', 'notification_utils', 'nu_db_admin', '210jidojdxwq9223HAdas')
 	user_id = auth_query(fullname='lalala', password='lalala', connection=conn)
 	# if user_id is None:
